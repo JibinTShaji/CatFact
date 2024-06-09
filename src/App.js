0cd9e1c2de36f325db7675b1a307770a4e282aa4
JibinTShaji <jibinshaji616@gmail.com>
2024-06-09 16:29:16 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ function App() {
   },[])
   return (
 <div className="flex w-full h-screen  justify-center bg-slate-900 items-center ">
-<div className="flex-row  rounded-xl py-32 w-1/2 bg-slate-800">
+<div className="flex-row  rounded-xl py-10 lg:py-32 lg:w-1/2 w-3/4 bg-slate-800">
 <div className="flex justify-center   px-5 py-5">
-  <div className="flex  text-gray-200 text-wrap text-xl font-semibold">{catFact}</div>
+  <div className="flex  text-gray-200 text-wrap lg:text-xl text-md font-semibold">{catFact}</div>
   </div>
   <div className="flex justify-center mt-2 ">
   <button className="flex   hover:bg-sky-600 active:bg-sky-800 bg-sky-500 text-gray-100 rounded-lg px-5 py-2" onClick={fetchData}>Generate Fact</button>
