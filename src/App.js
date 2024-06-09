23d080cbcb31b7b65cc279230f3324eb9c4841c5
JibinTShaji <jibinshaji616@gmail.com>
2024-06-09 11:35:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios"
+
 
 function App() {
+
+  const [catFact,setFact]=useState("")
+
+  const fetchData=async()=>{
+    const {data}=await axios.get("https://catfact.ninja/fact")
+    setFact(data.fact)
+
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="flex w-full h-screen  justify-center bg-slate-900 items-center ">
+<div className="flex-row  rounded-xl py-32 w-1/2 bg-slate-800">
+<div className="flex justify-center   px-5 py-5">
+  <div className="flex  text-gray-200 text-wrap text-xl font-semibold">{catFact}</div>
+  </div>
+  <div className="flex justify-center mt-2 ">
+  <button className="flex   hover:bg-sky-600 active:bg-sky-800 bg-sky-500 text-gray-100 rounded-lg px-5 py-2" onClick={fetchData}>Generate Fact</button>
+  </div>
+</div>
+</div>
   );
 }
 
 export default App;
+
